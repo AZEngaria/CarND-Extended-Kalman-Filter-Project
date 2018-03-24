9add9065ef5b149342b0579b6465c8c2543ab6a3
Aftab Engaria <aftabengaria@gmail.com>
2018-03-24 08:02:26 +0530

    Add files via upload

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using std::vector;
-
+#define PI 3.14159265
 /*
  * Constructor.
  */
@@ -21,7 +21,7 @@ FusionEKF::FusionEKF() {
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
   Hj_ = MatrixXd(3, 4);
-
+   
   //measurement covariance matrix - laser
   R_laser_ << 0.0225, 0,
         0, 0.0225;
@@ -31,12 +31,19 @@ FusionEKF::FusionEKF() {
         0, 0.0009, 0,
         0, 0, 0.09;
 
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
   /**
   TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
-  */
-
+  */	
+  ekf_.F_ = MatrixXd(4,4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+	
 
 }
 
@@ -62,19 +69,39 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
-
+	
+    ekf_.P_ = MatrixXd(4,4);
+    ekf_.P_ << 1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 100, 0,
+                0, 0, 0, 100;
+			  
+	cout << "P_ " << ekf_.P_;		  
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+	  	
+	  float rho = measurement_pack.raw_measurements_(0);
+	  float phi = measurement_pack.raw_measurements_(1);
+	  //ekf_.x_ << cos(phi * PI/180.0)*rho, sin(phi * PI/180.0)*rho, 0.0, 0.0;
+    ekf_.x_ << cos(phi)*rho, sin(phi)*rho, 0.0, 0.0;
+	  
+	  cout << "ekf_.x_ RADAR: " << ekf_.x_ <<endl;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+	    
+  		ekf_.x_ << measurement_pack.raw_measurements_(0), measurement_pack.raw_measurements_(1), 0.0, 0.0;  		
+  		
+      cout << "ekf_.x_ LASER: " << ekf_.x_ <<endl;
     }
 
     // done initializing, no need to predict or update
+    previous_timestamp_ = measurement_pack.timestamp_;
+    cout<< "First timestamp: " << previous_timestamp_ << endl;
     is_initialized_ = true;
     return;
   }
@@ -90,8 +117,28 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+    float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  	previous_timestamp_ = measurement_pack.timestamp_;
+  	float noise_ax = 9;
+  	float noise_ay = 9;
+  	float dt_2 = dt * dt;
+  	float dt_3 = dt_2 * dt;
+  	float dt_4 = dt_3 * dt;
+  	
+  	//Modify the F matrix so that the time is integrated
+  	
+  	ekf_.F_(0, 2) = dt;
+  	ekf_.F_(1, 3) = dt;
+  	cout << "F: " << ekf_.F_<< endl;		
+  	//set the process covariance matrix Q
+  	ekf_.Q_ = MatrixXd(4, 4);
+  	ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+  			   0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+  			   dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+  			   0, dt_3/2*noise_ay, 0, dt_2*noise_ay;	
 
-  ekf_.Predict();
+  	
+  	ekf_.Predict();
 
   /*****************************************************************************
    *  Update
@@ -105,8 +152,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+	
+  	Tools tools;
+  	Hj_ = tools.CalculateJacobian(ekf_.x_);	
+  	ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, Hj_, R_radar_, ekf_.Q_);
+  	ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
-    // Laser updates
+    // Laser updates	
+  	ekf_.Init(ekf_.x_, ekf_.P_, ekf_.F_, H_laser_, R_laser_, ekf_.Q_);
+  	ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,5 @@
 #include "kalman_filter.h"
-
+#include <iostream>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,11 +35,58 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+ 
+	VectorXd z_pred(3);
+	
+	float a = sqrt(x_(0)*x_(0) + x_(1)*x_(1));	
+	float b = atan2(x_(1), x_(0));
+	float c = 0;
+	if(fabs(a) > 0.0001){
+			
+		c = (x_(0)*x_(2) + x_(1)*x_(3))/ a;
+	}
+
+
+	z_pred << a, b, c;	
+	VectorXd y = z - z_pred;
+
+	/*The Kalman filter is expecting small angle values between the range -pi and pi
+	  Normalizing the angle phi so it will always stays be between -Pi and +Pi . */
+	
+	  while (y[1] > 3.14){
+	     y[1] = y[1] - (2 * 3.14);
+	  }
+	  while (y[1] < - 3.14){
+	     y[1] = y[1] + (2 * 3.14);
+	  }
+
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
 }
+
